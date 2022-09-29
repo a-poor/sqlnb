dc508869206db574b5f0567510e4819c99f176c2
a-poor <45295232+a-poor@users.noreply.github.com>
2022-09-29 14:49:54 -0700

    Adding cell runner

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-
 use serde_json::Value;
 use serde::{Serialize, Deserialize};
 
@@ -9,58 +7,46 @@ pub struct Client {
     /// Notebooks the client is currently managing.
     /// 
     /// Maps notebook filepaths to notebook objects.
-    pub notebooks: Arc<Mutex<HashMap<String, Box<ActiveNotebook>>>>,
+    pub notebooks: HashMap<String, Box<ActiveNotebook>>,
 
     /// A counter for creating new notebook IDs.
     /// 
     /// For example, a new notebook name may be of
     /// the form "notebook-{{ nb_inc }}.sql.nb"
-    pub nb_inc: Arc<Mutex<usize>>,
+    pub nb_inc: usize,
 }
 
 impl Client {
     /// Create a new instance of Client.
     pub fn new() -> Client {
         Client {
-            notebooks: Arc::new(Mutex::new(HashMap::new())),
-            nb_inc: Arc::new(Mutex::new(1)),
+            notebooks: HashMap::new(),
+            nb_inc: 0,
         }
     }
 
-    pub fn does_notebook_exist(&self, name: &str) -> Result<bool, String> {
-        // Get the notebooks.
-        let nbs = if let Ok(nbs) = self.notebooks.lock() {
-            nbs
-        } else {
-            return Err("Couldn't obtain lock for notebook data".to_string());
-        };
-
-        // Return true if the notebook exists.
-        Ok(nbs.contains_key(name))
+    /// Checks if a notebook with the given name exists
+    /// in the client's notebook map.
+    pub fn does_notebook_exist(&self, name: &str) -> bool {
+        self.notebooks.contains_key(name)
     }
 
     /// Return a new notebook name, using the client's
     /// notebook counter.
-    pub fn get_new_nb_name(&self) -> Result<String, String> {
-        // Get the client's notebook counter.
-        let mut nb_inc = if let Ok(nb_inc) = self.nb_inc.lock() {
-            nb_inc
-        } else {
-            return Err("Failed to lock nb_inc".to_string());
-        };
-
+    pub fn get_new_nb_name(&mut self) -> String {
         // Format the notebook name, using the counter.
-        let nb_id = format!("notebook-{}.sql.nb", *nb_inc);
+        let nb_id = format!("notebook-{}.sql.nb", self.nb_inc);
 
         // Increment the counter.
-        *nb_inc += 1;
+        self.nb_inc += 1;
 
         // Return the notebook name.
-        Ok(nb_id)
+        nb_id
     }
 }
 
 /// A notebook's save-state, on disk.
+#[derive(PartialEq, Debug)]
 pub enum SaveSate {
     /// The notebook has never been saved.
     NeverSaved,
@@ -204,6 +190,186 @@ impl ActiveNotebook {
         f(conn)
     }
 
+    /// Serialize the notebook's data to JSON.
+    pub fn to_json(&self) -> Result<String, String> {
+        serde_json::to_string_pretty(&self.data)
+            .map_err(|e| e.to_string())
+    }
+
+    /// Serialize the notebook's data to JSON and write it
+    /// to the notebook's file (`.nbpath`).
+    pub fn save_nb_data(&mut self) -> Result<(), String> {
+        // Serialize the notebook's data to JSON.
+        let json = self.to_json()?;
+
+        // Write the JSON to the notebook file.
+        std::fs::write(&self.nbpath, json)
+            .map_err(|e| e.to_string())?;
+
+        // Set the save state to clean.
+        self.save_state = SaveSate::Clean;
+
+        // Return success.
+        Ok(())
+    }
+
+    // List DB tables...
+
+    // Get DB table schema...
+
+    /// Insert a cell into the notebook cell list.
+    pub fn insert_cell(&mut self, pos: usize, cell: Cell) -> Result<(), String> {
+        // Check that the position is valid.
+        if pos > self.data.cells.len() {
+            return Err("Invalid cell index".to_string());
+        }
+
+        // Insert the cell.
+        self.data.cells.insert(pos, cell);
+
+        // Set the save state to dirty.
+        if self.save_state != SaveSate::NeverSaved {
+            self.save_state = SaveSate::Dirty;
+        }
+
+        // Return success.
+        Ok(())
+    }
+
+    /// Update a cell in the notebook cell list.
+    pub fn update_cell(&mut self, pos: usize, cell: Cell) -> Result<(), String> {
+        // Check that the position is valid.
+        if pos >= self.data.cells.len() {
+            return Err("Invalid cell index".to_string());
+        }
+
+        // Update the cell.
+        self.data.cells[pos] = cell;
+
+        // Set the save state to dirty.
+        if self.save_state != SaveSate::NeverSaved {
+            self.save_state = SaveSate::Dirty;
+        }
+
+        // Return success.
+        Ok(())
+    }
+
+    /// Run a query cell in the notebook.
+    pub fn run_cell(&mut self, pos: usize) -> Result<(), String> {
+        // Check that the position is valid.
+        if pos >= self.data.cells.len() {
+            return Err("Invalid cell index".to_string());
+        }
+
+        // Get the cell.
+        let cell = &self.data.cells[pos];
+
+        // Run the cell.
+        match cell {
+            Cell::Query(cell) => {
+                // Get the query
+                let query = cell.query.as_str();
+
+                // Get the db connection
+                let conn = if let Some(conn) = &mut self.connection {
+                    conn
+                } else {
+                    return Err("Failed to get connection".to_string());
+                };
+
+                // Create a place to store the results.
+                let mut results: Vec<HashMap<String, Option<String>>> = Vec::new();
+
+                // Mark the start time
+                let start = std::time::Instant::now();
+
+                // Run the query and pull out the results.
+                conn.iterate(query, |row| {
+                    // Create a container for the row
+                    let mut row_data: HashMap<String, Option<String>> = HashMap::new();
+
+                    for &(col, val) in row.iter() {
+                        let v = if let Some(v) = val {
+                            Some(v.to_string())
+                        } else {
+                            None
+                        };
+
+                        row_data.insert(col.to_string(), v);
+                    }
+
+                    // Add the row to the results.
+                    results.push(row_data);
+
+                    true
+                }).map_err(|e| e.to_string())?;
+
+                // Get the query duration.
+                let duration = start.elapsed();
+
+                // Store the results in the cell.
+                let mut cell = cell.clone();
+                cell.results = Some(results);
+                cell.query_time = Some(duration.as_millis());
+
+                // Update the cell.
+                self.update_cell(pos, Cell::Query(cell))?;
+
+                // Mark as dirty (if needed).
+                // (Note: This isn't needed, since `update_cell` does it already
+                // but it's here for consistency.)
+                if self.save_state != SaveSate::NeverSaved {
+                    self.save_state = SaveSate::Dirty;
+                }
+            },
+            _ => {
+                return Err("Can't run non-SQL cell".to_string());
+            },
+        }
+
+        // Return success.
+        Ok(())
+    }
+
+    /// Swap two cells in the notebook cell list.
+    pub fn swap_cells(&mut self, pos1: usize, pos2: usize) -> Result<(), String> {
+        // Check that the positions are valid.
+        if pos1 >= self.data.cells.len() || pos2 >= self.data.cells.len() {
+            return Err("Invalid cell index".to_string());
+        }
+
+        // Swap the cells.
+        self.data.cells.swap(pos1, pos2);
+
+        // Set the save state to dirty.
+        if self.save_state != SaveSate::NeverSaved {
+            self.save_state = SaveSate::Dirty;
+        }
+
+        // Return success.
+        Ok(())
+    }
+
+    /// Remove a cell from the notebook cell list.
+    pub fn delete_cell(&mut self, pos: usize) -> Result<(), String> {
+        // Check that the position is valid.
+        if pos >= self.data.cells.len() {
+            return Err("Invalid cell index".to_string());
+        }
+
+        // Delete the cell.
+        self.data.cells.remove(pos);
+
+        // Set the save state to dirty.
+        if self.save_state != SaveSate::NeverSaved {
+            self.save_state = SaveSate::Dirty;
+        }
+
+        // Return success.
+        Ok(())
+    }
+
 }
 
 #[derive(Serialize, Deserialize, Default)]
@@ -235,19 +401,58 @@ pub enum Cell {
     Markdown(MarkdownCell),
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct MarkdownCell {
     /// The cell's markdown contents
     pub contents: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+impl MarkdownCell {
+    /// Create a new markdown cell.
+    pub fn new() -> MarkdownCell {
+        MarkdownCell {
+            ..Default::default()
+        }
+    }
+
+    /// Set the cell's markdown contents.
+    pub fn with_content(&mut self, content: String) -> &mut MarkdownCell {
+        self.contents = content;
+        self
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct QueryCell {
     /// The cell's query contents
     pub query: String,
 
     /// The cell's query results
-    pub results: Option<HashMap<String, Vec<Value>>>,
+    /// 
+    /// This is a vector of rows, where each row is a map of column 
+    /// name to value. The value is an `Option<String>` because the 
+    /// value may be `NULL` and because it has already been formatted
+    /// as a string (except for nulls).
+    pub results: Option<Vec<HashMap<String, Option<String>>>>,
+
+    /// Amount of time, in milliseconds, that the
+    /// query took to execute.
+    pub query_time: Option<u128>,
+}
+
+impl QueryCell {
+    /// Create a new query cell.
+    pub fn new() -> QueryCell {
+        QueryCell {
+            ..Default::default()
+        }
+    }
+
+    /// Set the query contents of the cell.
+    pub fn with_query(&mut self, query: String) -> &mut QueryCell {
+        self.query = query;
+        self
+    }
 }
 
 
@@ -275,21 +480,21 @@ mod tests {
     #[test]
     fn new_client() {
         let client = super::Client::new();
-        assert_eq!(client.notebooks.lock().unwrap().len(), 0);
-        assert_eq!(*client.nb_inc.lock().unwrap(), 1);
+        assert_eq!(client.notebooks.len(), 0);
+        assert_eq!(client.nb_inc, 1);
     }
 
     #[test]
     fn get_new_nb_name() {
         // Create a client instance
-        let client = super::Client::new();
+        let mut client = super::Client::new();
 
         // Get a new notebook name
-        let nb_name = client.get_new_nb_name().unwrap();
+        let nb_name = client.get_new_nb_name();
         assert_eq!(nb_name, "notebook-1.sql.nb");
         
         // Get another new notebook name
-        let nb_name = client.get_new_nb_name().unwrap();
+        let nb_name = client.get_new_nb_name();
         assert_eq!(nb_name, "notebook-2.sql.nb");
     }
 
